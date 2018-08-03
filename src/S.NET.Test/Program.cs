3002357db1483dc63b88b0127b5fefa757d8b786
Ivan Chensky <i.chensky@ameria.de>
2018-08-03 19:05:39 +0300

    add support of generic lists with multiple types

--- src/S.NET.Test/Program.cs
+++ src/S.NET.Test/Program.cs
@@ -9,9 +9,13 @@ using System.Threading.Tasks;
 
 namespace S.NET.Test
 {
+    class Cap {
+        public string Name { get; set; }
+        public int Years { get; set; }
+    }
     class Kk {
-        public int MyProperty { get; set; }
-       // public List<List<int>> Vv { get; set; }
+       // public int MyProperty { get; set; }
+        public List<Cap> Vv { get; set; }
     }
     class Program
     {
@@ -19,12 +23,21 @@ namespace S.NET.Test
         static void Main(string[] args)
         {
             var str = File.ReadAllText("settings.clj");
-            var settings = SConvert.DeserializeObject<Settings>(str);
+            //var settings = SConvert.DeserializeObject<Settings>(str);
 
 
             //var settings = SConvert.DeserializeObject<List<int>>("(1 2 3)");
 
-            var rr = SConvert.SerializeObject(new Kk { MyProperty=324 });
+            //var dict = new Dictionary<string, Kk>();
+            //dict.Add("keyyy", new Kk { MyProperty = 24 });
+
+
+            
+
+            var rr = SConvert.SerializeObject( new Kk() { Vv = new List<Cap> {
+                new Cap{ Name="Bob" },
+                new Cap{ Name="Carl" }
+            } });
         }
     }
 }
